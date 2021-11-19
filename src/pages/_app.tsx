c2bdaa987092c75303c15029bab4141d606512e1
MateusJSouza <mateusjdev@gmail.com>
2021-11-19 11:59:58 -0300

    Estilos globais do app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app' // props que o componente pode receber
 
+import '../styles/global.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
