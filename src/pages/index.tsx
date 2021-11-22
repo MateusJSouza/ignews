6a68f1a8d85075e2ffa30057f5aca24be58bbb68
MateusJSouza <mateusjdev@gmail.com>
2021-11-22 18:18:09 -0300

    Utilizando static site generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   // Listando todos os preços
   const price = await stripe.prices.retrieve('price_1JyhZGB6IZAs4kldvwePcqK0')
   
@@ -48,9 +52,11 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }).format(price.unit_amount / 100),
   }
 
+  // revalidate -> revalidar o conteúdo da página a cada x minutos
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
