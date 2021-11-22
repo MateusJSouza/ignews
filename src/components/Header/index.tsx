208f1f112458205c459c79691c9ae5c31465bf22
MateusJSouza <mateusjdev@gmail.com>
2021-11-22 15:16:50 -0300

    Criação do componente signInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 
+import { SignInButton } from '../SignInButton';
+
 export function Header() {
   return (
     <header className={styles.header}>
@@ -9,6 +11,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a className={styles.active}>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
