a5e72b859caa53f6c5edb126b161faac0d9d0e6e
MateusJSouza <mateusjdev@gmail.com>
2022-02-08 19:31:03 -0300

    Criando a página de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styles from './styles.module.scss'
+
+export function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+    </>
+  );
+}
