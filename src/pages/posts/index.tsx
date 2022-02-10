903e2817238a50f20280c7be9eef0f819a871270
MateusJSouza <mateusjdev@gmail.com>
2022-02-10 16:04:32 -0300

    Página de posts construída

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,9 @@
+import { GetStaticProps } from 'next';
+import { getPrismicClient } from '../../services/prismic';
+
+import Prismic from '@prismicio/client'
 import Head from 'next/head';
+
 import styles from './styles.module.scss'
 
 export default function Posts() {
@@ -32,4 +37,22 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'publication'),
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+
+  })
+
+  console.log(JSON.stringify(response, null, 2))
+  
+  return {
+    props: {}
+  }
 }
